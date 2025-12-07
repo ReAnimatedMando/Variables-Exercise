e68f1f1f9e2c2ea0b587db0c44a0954efca3279b
Aaron Tanguma <the_guma@yahoo.com>
2025-12-07 11:20:53 -0700

    Declared and initialezed string, int, char, bool, double, and decimal variables. Used string interpolation to create a message to the console.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Aaron Tanguma"; // declare and initialize name variable type string
+
+            int age = 40; // declare and initialize age variable type int
+
+            char gender = 'm'; // declare and initialize gender variable type char
+
+            bool isStudent = true; // declare and initialize fact type variable isStudent
+
+            double average = 98.43; // declare and initialize average variable type double
+
+            decimal myAccount = 3400.33m; // declare and initialize myAccount variable type decimal
+            
+            Console.WriteLine($"Hi, I'm {name}! I am a {age} year old {gender}!"); // use variables to create a message to the console.
+            
+            // I notice that my IDE points to variables that haven't been used by underlining them in yellow.
         }
     }
 }
